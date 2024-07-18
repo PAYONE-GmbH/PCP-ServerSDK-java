e9a695a49eda7fadaed53de1bd8cbacefc450cc7
Blackfaded <r.heinen@nanogiants.de>
2024-07-18 10:46:12 +0200

    feat: add ordermanagement and payment endpoints

--- lib/src/main/java/com/payone/commerce/platform/lib/endpoints/OrderManagementCheckoutActionsApiClient.java
+++ lib/src/main/java/com/payone/commerce/platform/lib/endpoints/OrderManagementCheckoutActionsApiClient.java
@@ -6,36 +6,45 @@ package com.payone.commerce.platform.lib.endpoints;
 
 import java.io.IOException;
 import java.security.InvalidKeyException;
-import java.util.List;
-import java.util.Map;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.payone.commerce.platform.lib.ApiResponseException;
 import com.payone.commerce.platform.lib.CommunicatorConfiguration;
-import com.payone.commerce.platform.lib.models.CommerceCaseResponse;
-import com.payone.commerce.platform.lib.models.CreateCommerceCaseRequest;
-import com.payone.commerce.platform.lib.models.CreateCommerceCaseResponse;
-import com.payone.commerce.platform.lib.models.Customer;
+import com.payone.commerce.platform.lib.models.CancelRequest;
+import com.payone.commerce.platform.lib.models.CancelResponse;
+import com.payone.commerce.platform.lib.models.DeliverRequest;
+import com.payone.commerce.platform.lib.models.DeliverResponse;
 import com.payone.commerce.platform.lib.models.ErrorResponse;
-import com.payone.commerce.platform.lib.models.GetCommerceCasesQuery;
+import com.payone.commerce.platform.lib.models.OrderRequest;
+import com.payone.commerce.platform.lib.models.OrderResponse;
+import com.payone.commerce.platform.lib.models.ReturnRequest;
+import com.payone.commerce.platform.lib.models.ReturnResponse;
 
 import okhttp3.HttpUrl;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class CommerceCaseApiClient extends BaseApiClient {
+public class OrderManagementCheckoutActionsApiClient extends BaseApiClient {
 
-    public CommerceCaseApiClient(CommunicatorConfiguration config) throws InvalidKeyException {
+    public OrderManagementCheckoutActionsApiClient(CommunicatorConfiguration config) throws InvalidKeyException {
         super(config);
     }
 
-    public CreateCommerceCaseResponse createCommerceCaseRequest(String merchantId, CreateCommerceCaseRequest payload)
+    public OrderResponse createOrder(String merchantId, String commerceCaseId,
+            String checkoutId, OrderRequest payload)
             throws ApiResponseException, IOException {
         if (merchantId == null) {
             throw new IllegalArgumentException("Merchant ID is required");
+        }
+        if (commerceCaseId == null) {
+            throw new IllegalArgumentException("Commerce Case ID is required");
+        }
+        if (checkoutId == null) {
+            throw new IllegalArgumentException("Checkout ID is required");
+        }
+        if (payload == null) {
+            throw new IllegalArgumentException("Payload is required");
         }
 
         HttpUrl url = new HttpUrl.Builder()
@@ -44,6 +53,10 @@ public class CommerceCaseApiClient extends BaseApiClient {
                 .addPathSegment("v1")
                 .addPathSegment(merchantId)
                 .addPathSegment("commerce-cases")
+                .addPathSegment(commerceCaseId)
+                .addPathSegment("checkouts")
+                .addPathSegment(checkoutId)
+                .addPathSegment("order")
                 .build();
 
         String jsonString = getJsonMapper().writeValueAsString(payload);
@@ -68,114 +81,26 @@ public class CommerceCaseApiClient extends BaseApiClient {
                 ErrorResponse error = getJsonMapper().readValue(response.body().string(), ErrorResponse.class);
                 throw new ApiResponseException(error);
             }
-            return getJsonMapper().readValue(response.body().string(), CreateCommerceCaseResponse.class);
+            return getJsonMapper().readValue(response.body().string(), OrderResponse.class);
         } catch (JsonMappingException e) {
             throw new RuntimeException("Excepted valid JSON response, but failed to parse", e);
         }
-
     }
 
-    public List<CommerceCaseResponse> getAllCommerceCaseRequest(String merchantId)
+    public DeliverResponse deliverOrder(String merchantId, String commerceCaseId,
+            String checkoutId, DeliverRequest payload)
             throws ApiResponseException, IOException {
-        return getAllCommerceCaseRequest(merchantId, null);
-    }
-
-    public List<CommerceCaseResponse> getAllCommerceCaseRequest(String merchantId,
-            GetCommerceCasesQuery queryParams) throws ApiResponseException, IOException {
-        if (merchantId == null) {
-            throw new IllegalArgumentException("Merchant ID is required");
-        }
-
-        HttpUrl.Builder urlBuilder = new HttpUrl.Builder()
-                .scheme("https")
-                .host(this.getConfig().getHost())
-                .addPathSegment("v1")
-                .addPathSegment(merchantId)
-                .addPathSegment("commerce-cases");
-
-        if (queryParams != null) {
-            for (Map.Entry<String, String> entry : queryParams.toQueryMap().entrySet()) {
-                urlBuilder.addQueryParameter(entry.getKey(), entry.getValue());
-            }
-        }
-        HttpUrl url = urlBuilder.build();
-        System.out.println(url.toString());
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .build();
-
-        request = this.getRequestHeaderGenerator().generateAdditionalRequestHeaders(request);
-
-        Response response = this.getClient().newCall(request).execute();
-
-        try {
-            if (!response.isSuccessful()) {
-                if (response.code() != 400) {
-                    throw new RuntimeException("Api error: " + response.code());
-                }
-                ErrorResponse error = getJsonMapper().readValue(response.body().string(), ErrorResponse.class);
-                throw new ApiResponseException(error);
-            }
-            return getJsonMapper().readValue(response.body().string(),
-                    new TypeReference<List<CommerceCaseResponse>>() {
-                    });
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Excepted valid JSON response, but failed to parse", e);
-        }
-
-    }
-
-    public CommerceCaseResponse getCommerceCaseRequest(String merchantId,
-            String commerceCaseId) throws ApiResponseException, IOException {
         if (merchantId == null) {
             throw new IllegalArgumentException("Merchant ID is required");
         }
         if (commerceCaseId == null) {
             throw new IllegalArgumentException("Commerce Case ID is required");
         }
-
-        HttpUrl.Builder urlBuilder = new HttpUrl.Builder()
-                .scheme("https")
-                .host(this.getConfig().getHost())
-                .addPathSegment("v1")
-                .addPathSegment(merchantId)
-                .addPathSegment("commerce-cases")
-                .addPathSegment(commerceCaseId);
-
-        HttpUrl url = urlBuilder.build();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .build();
-
-        request = this.getRequestHeaderGenerator().generateAdditionalRequestHeaders(request);
-
-        Response response = this.getClient().newCall(request).execute();
-
-        try {
-            if (!response.isSuccessful()) {
-                if (response.code() != 400) {
-                    throw new RuntimeException("Api error: " + response.code());
-                }
-                ErrorResponse error = getJsonMapper().readValue(response.body().string(), ErrorResponse.class);
-                throw new ApiResponseException(error);
-            }
-            return getJsonMapper().readValue(response.body().string(), CommerceCaseResponse.class);
-        } catch (JsonMappingException e) {
-            throw new RuntimeException("Excepted valid JSON response, but failed to parse", e);
+        if (checkoutId == null) {
+            throw new IllegalArgumentException("Checkout ID is required");
         }
-
-    }
-
-    public void updateCommerceCaseRequest(String merchantId, String commerceCaseId,
-            Customer payload) throws IOException, ApiResponseException {
-        if (merchantId == null) {
-            throw new IllegalArgumentException("Merchant ID is required");
-        }
-        if (commerceCaseId == null) {
-            throw new IllegalArgumentException("Commerce Case ID is required");
+        if (payload == null) {
+            throw new IllegalArgumentException("Payload is required");
         }
 
         HttpUrl url = new HttpUrl.Builder()
@@ -185,26 +110,25 @@ public class CommerceCaseApiClient extends BaseApiClient {
                 .addPathSegment(merchantId)
                 .addPathSegment("commerce-cases")
                 .addPathSegment(commerceCaseId)
+                .addPathSegment("checkouts")
+                .addPathSegment(checkoutId)
+                .addPathSegment("deliver")
                 .build();
 
-        String jsonString = null;
-        try {
-            jsonString = getJsonMapper().writeValueAsString(payload);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to encode payload as json", e);
-        }
+        String jsonString = getJsonMapper().writeValueAsString(payload);
 
-        RequestBody formBody = RequestBody.create("{\"customer\":" + jsonString + "}", JSON);
+        RequestBody formBody = RequestBody.create(jsonString, JSON);
 
         Request request = new Request.Builder()
                 .url(url)
-                .patch(formBody)
+                .post(formBody)
                 .header("Content-Type", formBody.contentType().toString())
                 .build();
 
         request = this.getRequestHeaderGenerator().generateAdditionalRequestHeaders(request);
 
         Response response = this.getClient().newCall(request).execute();
+
         try {
             if (!response.isSuccessful()) {
                 if (response.code() != 400) {
@@ -213,10 +137,122 @@ public class CommerceCaseApiClient extends BaseApiClient {
                 ErrorResponse error = getJsonMapper().readValue(response.body().string(), ErrorResponse.class);
                 throw new ApiResponseException(error);
             }
-
+            return getJsonMapper().readValue(response.body().string(), DeliverResponse.class);
         } catch (JsonMappingException e) {
             throw new RuntimeException("Excepted valid JSON response, but failed to parse", e);
         }
-
     }
+
+    public ReturnResponse returnOrder(String merchantId, String commerceCaseId,
+            String checkoutId, ReturnRequest payload)
+            throws ApiResponseException, IOException {
+        if (merchantId == null) {
+            throw new IllegalArgumentException("Merchant ID is required");
+        }
+        if (commerceCaseId == null) {
+            throw new IllegalArgumentException("Commerce Case ID is required");
+        }
+        if (checkoutId == null) {
+            throw new IllegalArgumentException("Checkout ID is required");
+        }
+        if (payload == null) {
+            throw new IllegalArgumentException("Payload is required");
+        }
+
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("https")
+                .host(this.getConfig().getHost())
+                .addPathSegment("v1")
+                .addPathSegment(merchantId)
+                .addPathSegment("commerce-cases")
+                .addPathSegment(commerceCaseId)
+                .addPathSegment("checkouts")
+                .addPathSegment(checkoutId)
+                .addPathSegment("return")
+                .build();
+
+        String jsonString = getJsonMapper().writeValueAsString(payload);
+
+        RequestBody formBody = RequestBody.create(jsonString, JSON);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .header("Content-Type", formBody.contentType().toString())
+                .build();
+
+        request = this.getRequestHeaderGenerator().generateAdditionalRequestHeaders(request);
+
+        Response response = this.getClient().newCall(request).execute();
+
+        try {
+            if (!response.isSuccessful()) {
+                if (response.code() != 400) {
+                    throw new RuntimeException("Api error: " + response.code());
+                }
+                ErrorResponse error = getJsonMapper().readValue(response.body().string(), ErrorResponse.class);
+                throw new ApiResponseException(error);
+            }
+            return getJsonMapper().readValue(response.body().string(), ReturnResponse.class);
+        } catch (JsonMappingException e) {
+            throw new RuntimeException("Excepted valid JSON response, but failed to parse", e);
+        }
+    }
+
+    public CancelResponse cancelOrder(String merchantId, String commerceCaseId,
+            String checkoutId, CancelRequest payload)
+            throws ApiResponseException, IOException {
+        if (merchantId == null) {
+            throw new IllegalArgumentException("Merchant ID is required");
+        }
+        if (commerceCaseId == null) {
+            throw new IllegalArgumentException("Commerce Case ID is required");
+        }
+        if (checkoutId == null) {
+            throw new IllegalArgumentException("Checkout ID is required");
+        }
+        if (payload == null) {
+            throw new IllegalArgumentException("Payload is required");
+        }
+
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("https")
+                .host(this.getConfig().getHost())
+                .addPathSegment("v1")
+                .addPathSegment(merchantId)
+                .addPathSegment("commerce-cases")
+                .addPathSegment(commerceCaseId)
+                .addPathSegment("checkouts")
+                .addPathSegment(checkoutId)
+                .addPathSegment("cancel")
+                .build();
+
+        String jsonString = getJsonMapper().writeValueAsString(payload);
+
+        RequestBody formBody = RequestBody.create(jsonString, JSON);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .header("Content-Type", formBody.contentType().toString())
+                .build();
+
+        request = this.getRequestHeaderGenerator().generateAdditionalRequestHeaders(request);
+
+        Response response = this.getClient().newCall(request).execute();
+
+        try {
+            if (!response.isSuccessful()) {
+                if (response.code() != 400) {
+                    throw new RuntimeException("Api error: " + response.code());
+                }
+                ErrorResponse error = getJsonMapper().readValue(response.body().string(), ErrorResponse.class);
+                throw new ApiResponseException(error);
+            }
+            return getJsonMapper().readValue(response.body().string(), CancelResponse.class);
+        } catch (JsonMappingException e) {
+            throw new RuntimeException("Excepted valid JSON response, but failed to parse", e);
+        }
+    }
+
 }
