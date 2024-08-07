7a7a5bc58cc8380701a668b78245c36ca6e63256
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-08-07 15:23:04 +0200

    feature: add integrator field to be send as part of the ServerMetaInfo

--- lib/src/main/java/com/payone/commerce/platform/lib/CommunicatorConfiguration.java
+++ lib/src/main/java/com/payone/commerce/platform/lib/CommunicatorConfiguration.java
@@ -4,11 +4,13 @@ public class CommunicatorConfiguration {
     private final String apiKey;
     private final String apiSecret;
     private final String host;
+    private final String integrator;
 
-    public CommunicatorConfiguration(String apiKey, String apiSecret, String host) {
+    public CommunicatorConfiguration(String apiKey, String apiSecret, String host, String integrator) {
         this.apiKey = apiKey;
         this.apiSecret = apiSecret;
         this.host = host;
+        this.integrator = integrator;
     }
 
     public String getApiKey() {
@@ -21,5 +23,9 @@ public class CommunicatorConfiguration {
 
     public String getHost() {
         return host;
+    }
+
+    public String getIntegrator() {
+        return integrator;
     }
 }
