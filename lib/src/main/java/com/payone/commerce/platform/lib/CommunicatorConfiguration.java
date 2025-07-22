112cef5bee991c0172d438ff55b671bab39fb361
Stefan Neidig <stefan@rpdev.net>
2025-07-22 09:26:53 +0200

    chore: add ok http client as configurable object

--- lib/src/main/java/com/payone/commerce/platform/lib/CommunicatorConfiguration.java
+++ lib/src/main/java/com/payone/commerce/platform/lib/CommunicatorConfiguration.java
@@ -2,17 +2,21 @@ package com.payone.commerce.platform.lib;
 
 import com.payone.commerce.platform.lib.utils.ServerMetaInfo;
 
+import okhttp3.OkHttpClient;
+
 public class CommunicatorConfiguration {
     private final String apiKey;
     private final String apiSecret;
     private final String host;
     private final ServerMetaInfo serverMetaInfo;
+    private OkHttpClient httpClient;
 
-    public CommunicatorConfiguration(String apiKey, String apiSecret, String host, String integrator) {
+        public CommunicatorConfiguration(String apiKey, String apiSecret, String host, String integrator) {
         this.apiKey = apiKey;
         this.apiSecret = apiSecret;
         this.host = host;
         this.serverMetaInfo = ServerMetaInfo.withDefaults(integrator);
+        this.httpClient = null;
     }
 
     public String getApiKey() {
@@ -29,5 +33,13 @@ public class CommunicatorConfiguration {
 
     public ServerMetaInfo getServerMetaInfo() {
         return serverMetaInfo;
+    }
+
+    public OkHttpClient getHttpClient() {
+        return httpClient;
+    }
+
+    public void setHttpClient(OkHttpClient httpClient) {
+        this.httpClient = httpClient;
     }
 }
